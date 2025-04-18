935ba3d30c3b8b5b6fe1ef529d475e9cbb97a03e
Ford Neild <hanfordn35@gmail.com>
2025-04-18 13:58:43 -0400

    bump helm chart version

--- internal/resources/crossplane_aws.go
+++ internal/resources/crossplane_aws.go
@@ -7,6 +7,12 @@ import (
 	"github.com/fordneild/omega/internal/project"
 )
 
+const (
+	// only used for EKS provider for now
+	CrossplaneAwsProviderVersion = "v1.21.1"
+	CrossplaneHelmChartVersion   = "1.19.1"
+)
+
 func NewAWSCrossplane(project project.Project) cdk8s.App {
 	app := cdk8s.NewApp(nil)
 	crossplaneAwsChart := cdk8s.NewChart(app, project.GetSubId("chart"), nil)
@@ -18,7 +24,7 @@ func NewAWSCrossplane(project project.Project) cdk8s.App {
 		ProjectName:            jsii.String(project.GetId()),
 		ChartName:              jsii.String("crossplane"),
 		ChartRepoUrl:           jsii.String("https://charts.crossplane.io/stable"),
-		ChartTargetRevision:    jsii.String("1.10.0"),
+		ChartTargetRevision:    jsii.String(CrossplaneHelmChartVersion),
 		ReleaseName:            jsii.String("crossplane-system"),
 		DisableAutomatedSync:   jsii.Bool(false),
 		DisableCreateNamespace: jsii.Bool(false),
@@ -30,7 +36,7 @@ func NewAWSCrossplane(project project.Project) cdk8s.App {
 			Name: jsii.String("provider-aws"),
 		},
 		Spec: &crossplane.ProviderSpec{
-			Package: jsii.String("xpkg.crossplane.io/crossplane-contrib/provider-aws-eks:v1.21.1"),
+			Package: jsii.Sprintf("xpkg.crossplane.io/crossplane-contrib/provider-aws-eks:%s", CrossplaneAwsProviderVersion),
 		},
 	})
 	return app
