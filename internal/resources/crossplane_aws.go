61c459e338f73506a401d647bfd240ace85e5f98
Ford Neild <hanfordn35@gmail.com>
2025-04-18 17:55:16 -0400

    added ProviderConfig for crossplane

--- internal/resources/crossplane_aws.go
+++ internal/resources/crossplane_aws.go
@@ -3,6 +3,7 @@ package resources
 import (
 	"github.com/aws/jsii-runtime-go"
 	"github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
+	awsupboundio "github.com/fordneild/omega/imports/awsupboundio"
 	crossplane "github.com/fordneild/omega/imports/pkgcrossplaneio"
 	"github.com/fordneild/omega/internal/project"
 )
@@ -13,6 +14,7 @@ const (
 	CrossplaneHelmChartVersion   = "1.19.1"
 )
 
+// requires a secret to be created like so: https://docs.crossplane.io/v1.19/getting-started/provider-aws/#create-a-kubernetes-secret-with-the-aws-credentials
 func NewAWSCrossplane(project project.Project) cdk8s.App {
 	app := cdk8s.NewApp(nil)
 	crossplaneAwsChart := cdk8s.NewChart(app, project.GetSubId("chart"), nil)
@@ -37,6 +39,22 @@ func NewAWSCrossplane(project project.Project) cdk8s.App {
 		},
 		Spec: &crossplane.ProviderSpec{
 			Package: jsii.Sprintf("xpkg.crossplane.io/crossplane-contrib/provider-aws-eks:%s", CrossplaneAwsProviderVersion),
+		},
+	})
+	// Set up AWS EKS provider config
+	awsupboundio.NewProviderConfig(crossplaneAwsChart, project.GetSubId("provider-config-aws"), &awsupboundio.ProviderConfigProps{
+		Metadata: &cdk8s.ApiObjectMetadata{
+			Name: jsii.String("provider-config-aws"),
+		},
+		Spec: &awsupboundio.ProviderConfigSpec{
+			Credentials: &awsupboundio.ProviderConfigSpecCredentials{
+				Source: awsupboundio.ProviderConfigSpecCredentialsSource_SECRET,
+				SecretRef: &awsupboundio.ProviderConfigSpecCredentialsSecretRef{
+					Namespace: jsii.String("crossplane-system"),
+					Name:      jsii.String("aws-secret"),
+					Key:       jsii.String("creds"),
+				},
+			},
 		},
 	})
 	return app
